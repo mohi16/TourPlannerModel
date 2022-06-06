aaf34ef939628f87cfc6a8ea121511e3340ef415
mohi16 <75882258+mohi16@users.noreply.github.com>
2022-06-06 17:04:09 +0200

    finishing project

--- src/main/java/org/easytours/tpmodel/config/ConfigLoader.java
+++ src/main/java/org/easytours/tpmodel/config/ConfigLoader.java
@@ -2,6 +2,7 @@ package org.easytours.tpmodel.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.easytours.tpmodel.logging.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,10 +15,7 @@ public class ConfigLoader {
     public static <T> T load(Class<T> t, String path) throws IOException {
         File file = new File(path);
         if (!file.exists()) {
-            if (!file.createNewFile())
-            {
-                System.out.println("file already exists XD");
-            }
+            throw new IOException("No Config File");
         }
 
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
